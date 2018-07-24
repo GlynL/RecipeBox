7ff332b4d612c6028cad8fc3c964bcc797404775
Glyn L <glyn_92@hotmail.com>
2018-07-24 15:32:54 +1000

    added jwt to user routes

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,9 +1,20 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 exports.createUser = (req, res) => {
   User.create(req.body)
     .then(result => {
-      return res.json({ message: "User created successfully" });
+      let { id, username } = result;
+      let token = jwt.sign(
+        {
+          id,
+          username
+        },
+        process.env.SECRET_KEY
+      );
+      return res
+        .status(200)
+        .json({ message: "User created successfully", id, username, token });
     })
     .catch(err => {
       console.log("something went wrong");
@@ -12,8 +23,25 @@ exports.createUser = (req, res) => {
 };
 
 exports.login = async (req, res) => {
-  const user = await User.findOne({ username: req.body.username });
-  const isMatch = await user.comparePassword(req.body.password);
-  console.log(user);
-  return isMatch;
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    const isMatch = await user.comparePassword(req.body.password);
+    console.log(user);
+    if (isMatch) {
+      let token = jwt.sign(
+        {
+          id: user.id,
+          username: user.username
+        },
+        process.env.SECRET_KEY
+      );
+      return res.status(200).json({
+        id: user.id,
+        username: user.username,
+        token
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
